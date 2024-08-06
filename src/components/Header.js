ae51bed6cc187c608037e338329a6a0e6ee86c76
Shivam Ramdhani <ramdhanishivam7678@gmail.com>
2024-08-06 19:50:42 +0530

    login logout - useState

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+    const [loginOrLogout, setLoginOrLogout] = useState("Login");
     return (
         <div className="header">
             <div className="logo-container">
-                <a>
-                    <img className="logo-image" src={LOGO_URL} ></img>
-                </a>
+                <a><img className="logo-image" src={LOGO_URL} ></img></a>
             </div>
             <div className="navigation-items">
                 <ul>
@@ -14,6 +14,9 @@ const Header = () => {
                     <li>Home</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="login-button" onClick={(e) => {
+                        e.target.innerHTML == "Login" ? setLoginOrLogout("Logout") : setLoginOrLogout("Login")
+                    }}>{loginOrLogout}</button>
                 </ul>
             </div>
         </div>
