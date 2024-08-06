eadcd25f5f3384691e8e730109e6a76874fb6b45
Shivam Ramdhani <ramdhanishivam7678@gmail.com>
2024-08-06 13:27:47 +0530

    top rated resto - filter data - useState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,24 @@
 import RestaurantCard from "./RestaruantCard";
 import { resList } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+    const [listOfRes, setListOfRes] = useState(resList);
     return (
         <div className="body">
             <div className="search-bar">
                 <input type="text" placeholder="Enter the name"></input>
                 <button>Click to search</button>
+                <button onClick={()=>{
+                    const filteredList = listOfRes.filter(
+                        (res) => {
+                            return res.info.avgRating > 4.5
+                        });
+                    setListOfRes(filteredList);
+                }}>Top Rated Restaruants</button>
             </div>
             <div className="restaurant-list">
-                {resList.map((restaurant) => (
+                {listOfRes.map((restaurant) => (
                     <RestaurantCard key={restaurant.info.id} restaurantInfo={restaurant} ></RestaurantCard>
                 ))}
             </div>
