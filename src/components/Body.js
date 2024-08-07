96c47ea79234a67ecb404ef8e1a4bf3db37e5a8c
Shivam Ramdhani <ramdhanishivam7678@gmail.com>
2024-08-07 08:45:44 +0530

    search optimise - imp commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,39 +4,44 @@ import { API_URL } from "../utils/constants";
 
 const Body = () => {
     const [listOfRes, setListOfRes] = useState([]);
+    const [filterListOfRes, setFilterListOfRes] = useState([]);
     const [searchValue, setSearchValue] = useState("");
 
     useEffect(()=>{
         fetchData();
-    },[listOfRes])
+    },[])
 
     const fetchData = async () => {
         const data = await fetch(API_URL);
         const response = await data.json();
         setListOfRes(response.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+        setFilterListOfRes(response.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
     }
     return (
         <div className="body">
             <div className="search-bar">
-                <input type="text" value={searchValue} onChange={(e) => {setSearchValue(e.target.value)}} onKeyUp={(e)=>{
-                    let givenInput = e.target.value;
+                <input type="text" value={searchValue} onChange={(e) => {setSearchValue(e.target.value)}} placeholder="Enter the name"></input>
+                <button onClick={(e)=>{
+                    let givenInput = searchValue;
                     const filteredList = listOfRes.filter(
                         (res) => {
-                            return givenInput && res.info.name.toUpperCase().includes(givenInput.toUpperCase()) == true;
+                            return givenInput != "" && res.info.name.toUpperCase().includes(givenInput.toUpperCase()) == true;
                         });
-                    setListOfRes(filteredList);
-                }} placeholder="Enter the name"></input>
-                <button>Click to search</button>
+                    setFilterListOfRes(filteredList);
+                    if (givenInput == "") {
+                        setFilterListOfRes(listOfRes)
+                    }
+                }}>Click to search</button>
                 <button onClick={()=>{
                     const filteredList = listOfRes.filter(
                         (res) => {
                             return res.info.avgRating > 4.5
                         });
-                    setListOfRes(filteredList);
+                        setFilterListOfRes(filteredList);
                 }}>Top Rated Restaruants</button>
             </div>
             <div className="restaurant-list">
-                {listOfRes.map((restaurant) => (
+                {filterListOfRes.map((restaurant) => (
                     <RestaurantCard key={restaurant.info.id} restaurantInfo={restaurant} ></RestaurantCard>
                 ))}
             </div>
