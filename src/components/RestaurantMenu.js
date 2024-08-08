761bc7532979493c3ead047f374dc6b98d12f39b
Shivam Ramdhani <ramdhanishivam7678@gmail.com>
2024-08-08 19:39:20 +0530

    minor changes

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -17,7 +17,7 @@ const RestaurantMenu = () => {
     }
   return (
     <div className="menuCard" style={{backgroundImage:CDN_URL+menuData.cloudinaryImageId}}>
-        <img className="logo-image" src={ CDN_URL+menuData.cloudinaryImageId} ></img>
+        <img alt={menuData.name + "image"} className="logo-image" src={ CDN_URL+menuData.cloudinaryImageId} ></img>
         <h2>{menuData.name}</h2>
         <h3>{menuData.costForTwoMessage}</h3>
         <h2>{menuData.city}</h2>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 class UserClassChild extends React.Component {
     constructor() {
         super();
